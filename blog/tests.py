c0d8f6d17432b7867f05aec240fae7871c04621f
sepehreshraghisamani <sepehreshsa@gmail.com>
2025-08-20 05:27:03 -0700

    tests for list view and details view added

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Blog_Post
+from django.contrib.auth import get_user_model
+
 
 # Create your tests here.
+class BlogPostTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.post = Blog_Post.objects.create(
+            title='Test Post',
+            content='This is a test post.',
+            author=self.user
+        )
+
+    def test_blog_post_creation(self):
+        self.assertEqual(self.post.title, 'Test Post')
+        self.assertEqual(self.post.content, 'This is a test post.')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+
+    def test_blog_post_list_view(self):
+        response = self.client.get(reverse('blog-list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Post')
+        self.assertTemplateUsed(response, 'blog/blog_list.html')
+
+    def test_blog_post_detail_view(self):
+        response = self.client.get(reverse('blog-detail', args=[self.post.id]))
+        No_response = self.client.get(reverse('blog-detail', args=['1000']))
+        self.assertEqual(No_response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'This is a test post.')
+        self.assertTemplateUsed(response, 'blog/blog_detail.html') 
+
--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', BlogPostListView.as_view(), name='blog-list'),
-    path('/<int:pk>/', views.BlogPostDetailView.as_view(), name='blog-detail'),
+    path('<int:pk>/', views.BlogPostDetailView.as_view(), name='blog-detail'),
 ]
